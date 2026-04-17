3f1cbadfc6c614056a984f949182a9304739a8a1
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2026-04-17 08:15:04 +0700

    fix-img

--- src/main/java/ra/edu/miniprojectjavawebss10/repository/DeviceRepository.java
+++ src/main/java/ra/edu/miniprojectjavawebss10/repository/DeviceRepository.java
@@ -14,9 +14,9 @@ public class DeviceRepository {
     private final AtomicInteger idGenerator = new AtomicInteger(1);
 
     public DeviceRepository() {
-        save(new Device(0, "Laptop Dell G15", "https://via.placeholder.com/150?text=Dell+G15", 10));
-        save(new Device(0, "Máy chiếu Sony", "https://via.placeholder.com/150?text=Projector", 5));
-        save(new Device(0, "Bảng vẽ Wacom", "https://via.placeholder.com/150?text=Wacom", 15));
+        save(new Device(0, "Laptop Dell G15", "https://bizweb.dktcdn.net/100/408/235/files/dell-g5-5510-1.jpg?v=1635317802220", 10));
+        save(new Device(0, "Máy chiếu Sony", "https://maychieubacha.vn/temp/uploaded-may%20chieu-Sony-VPL%20EX230_SONY%20VPL-EX230%20(2)_rs1_600x412.jpeg", 5));
+        save(new Device(0, "Bảng vẽ Wacom", "https://tugia.vn/sites/default/files/bang-ve-may-tinh-wacom-cintiq-13hd-dtk-1301-k0-cx.jpg", 15));
     }
 
     public List<Device> findAll() {
